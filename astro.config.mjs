3fa1e4a179ec55f81b3a7d567b202588a6a2cbd1
Dmitriy Solokhin <disolokhin@gmail.com>
2026-04-22 11:53:08 -0500

    chore: add tailwind and base layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import { defineConfig } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   site: "https://solokhin.dev",
   adapter: cloudflare(),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
